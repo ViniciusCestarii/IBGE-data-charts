828168046b048cb5dbec784594be8eafc69702c6
Vinicius Cestari <viniciuscestari01@gmail.com>
2023-08-18 13:19:40 -0300

    update checkboxSpacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -222,7 +222,7 @@ function IBGEDataPage() {
             <span className='text-[0.65rem] text-center'>{dataInfo[dataOption].percentage && checkData(filteredData) ? "" : <><Cancel className='white' fontSize='small'/> | esse tipo de dado não suporta</>}</span>
           </>
         } placement='top'>
-            <div className='flex flex-row items-center justify-center w-full sm:w-auto sm:flex-col max-w-[340px]'>
+            <div className='flex flex-row items-center justify-center w-full sm:w-auto sm:flex-col max-w-[340px] space-x-2 sm:space-x-0'>
               <PercentBox style={{ color: dataInfo[dataOption].percentage && checkData(filteredData) ? 'white' : 'rgba(120, 120, 160, 0.7)' }} fontSize='medium' />
               <Checkbox
                 disabled={!(dataInfo[dataOption].percentage && checkData(filteredData))}
@@ -239,7 +239,7 @@ function IBGEDataPage() {
             <span className='text-[0.65rem] text-center'>{checkMaxYears(dataOption) && checkData(filteredData) ? "" : <><Cancel className='white' fontSize='small'/> | há poucos dados</>}</span>
           </>
         } placement='top'>
-            <div className='flex flex-row items-center justify-center w-full sm:w-auto sm:flex-col max-w-[340px]'>
+            <div className='flex flex-row items-center justify-center w-full sm:w-auto sm:flex-col max-w-[340px] space-x-2 sm:space-x-0'>
               <DivisionBox style={{ color: checkMaxYears(dataOption) && checkData(filteredData) ? 'white' : 'rgba(120, 120, 160, 0.7)' }} fontSize='medium' />
               <Checkbox
                 disabled={!(checkMaxYears(dataOption) && checkData(filteredData))}
@@ -249,7 +249,7 @@ function IBGEDataPage() {
             </div>
           </TooltipMUI>
           <TooltipMUI enterTouchDelay={0} leaveTouchDelay={5000} title={`${isContrast ? 'Diminuir contraste' : 'Aumentar contraste'}`} placement='top'>
-            <div className='flex flex-row items-center justify-center w-full sm:w-auto sm:flex-col max-w-[340px]'>
+            <div className='flex flex-row items-center justify-center w-full sm:w-auto sm:flex-col max-w-[340px] space-x-2 sm:space-x-0'>
               <StarFourPointsBox className='text-white' fontSize='medium' />
               <Checkbox
                 className={`${isContrast ? 'bg-gradient-to-br from-green-300 via-purple-500 to-orange-600' : ''}`}
